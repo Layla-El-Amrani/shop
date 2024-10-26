f30d65dd6c1d118e12bc0e167d7df956e2e2cf22
Layla-El-Amrani <160767348+Layla-El-Amrani@users.noreply.github.com>
2024-10-26 12:59:49 +0100

    Update main.jsx

--- src/Component/main.jsx
+++ src/Component/main.jsx
@@ -92,9 +92,9 @@ const Styledbutton = styled.button`
 
 export default function Main({ setCartItem, setCount }) {
   const images = [
-    `${process.env.PUBLIC_URL}/image/bg1.jpg`,
-    `${process.env.PUBLIC_URL}/image/bg2.jpg`,
-    `${process.env.PUBLIC_URL}/image/bg3.jpg`
+    "/image/bg1.jpg",
+    "/image/bg2.jpg",
+    "/image/bg3.jpg"
   ];
   
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -113,15 +113,15 @@ export default function Main({ setCartItem, setCount }) {
   };
 
   const products = [
-    { id: 1, name: "Montre Classique", price: "$150.00", img: `${process.env.PUBLIC_URL}/image/4.jpg` },
-    { id: 2, name: "Montre Sportive", price: "$200.00", img: `${process.env.PUBLIC_URL}/image/1.avif` },
-    { id: 3, name: "Montre de Luxe", price: "$800.00", img: `${process.env.PUBLIC_URL}/image/5.jpg` },
-    { id: 4, name: "Montre Étanche", price: "$120.00", img: `${process.env.PUBLIC_URL}/image/2.avif` },
-    { id: 5, name: "Montre Minimaliste", price: "$100.00", img: `${process.env.PUBLIC_URL}/image/4.avif` },
-    { id: 6, name: "Montre Connectée", price: "$250.00", img: `${process.env.PUBLIC_URL}/image/4.jpeg` },
-    { id: 7, name: "Montre en Bois", price: "$130.00", img: `${process.env.PUBLIC_URL}/image/4.jpg` },
-    { id: 8, name: "Montre Automatique", price: "$500.00", img: `${process.env.PUBLIC_URL}/image/5.jpg` },
-    { id: 9, name: "Montre Vintage", price: "$300.00", img: `${process.env.PUBLIC_URL}/image/6.jpg` },
+    { id: 1, name: "Montre Classique", price: "$150.00", img: "/image/4.jpg" },
+    { id: 2, name: "Montre Sportive", price: "$200.00", img: "/image/1.avif" },
+    { id: 3, name: "Montre de Luxe", price: "$800.00", img: "/image/5.jpg" },
+    { id: 4, name: "Montre Étanche", price: "$120.00", img: "/image/2.avif" },
+    { id: 5, name: "Montre Minimaliste", price: "$100.00", img: "/image/4.avif" },
+    { id: 6, name: "Montre Connectée", price: "$250.00", img: "/image/4.jpeg" },
+    { id: 7, name: "Montre en Bois", price: "$130.00", img: "/image/4.jpg" },
+    { id: 8, name: "Montre Automatique", price: "$500.00", img: "/image/5.jpg" },
+    { id: 9, name: "Montre Vintage", price: "$300.00", img: "/image/6.jpg" },
   ];
 
   return (
